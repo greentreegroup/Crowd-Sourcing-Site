08ab6a564dcbb03eb1cfea30899cb0e28a6035b8
hhalwan <hhalwan@umich.edu>
2024-03-01 14:49:03 -0500

    Added developer, target hold period, and interest rate to listings page. Removed property type

--- crowd-sourcing/src/components/InvestmentListings.js
+++ crowd-sourcing/src/components/InvestmentListings.js
@@ -9,7 +9,6 @@ const InvestmentListings = () => {
   const [minPrice, setMinPrice] = useState(-1);
   const [maxPrice, setMaxPrice] = useState(-1);
   const [sortByRecent, setSortByRecent] = useState(false);
-  const [propertyType, setPropertyType] = useState('none');
 
   const toggleFilter = () => {
     setShowFilter(!showFilter); // Toggle the state of showing filter
@@ -25,10 +24,6 @@ const InvestmentListings = () => {
 
   const handleSortByRecentChange = (e) => {
     setSortByRecent(e.target.checked);
-  };
-
-  const handlePropertyTypeChange = (e) => {
-    setPropertyType(e.target.value);
   };
 
   const reformatDate = (dateString) => {
@@ -49,7 +44,7 @@ const InvestmentListings = () => {
       console.log(`maxPrice: ${maxPrice}`);
 
       const response = await axios.get(
-        `https://prod-44.southeastasia.logic.azure.com/workflows/2dd568592654417e8216e170db743a61/triggers/manual/paths/invoke/min_price/${minPrice}/max_price/${maxPrice}/sort_by_recent/${sortByRecent}/property_type/${propertyType}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=xqoE7trIuqQ2NkKl4zVVv3kijiPo2GZqT6MqFZuiIsw`
+        `https://prod-44.southeastasia.logic.azure.com/workflows/2dd568592654417e8216e170db743a61/triggers/manual/paths/invoke/min_price/${minPrice}/max_price/${maxPrice}/sort_by_recent/${sortByRecent}/property_type/none?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=xqoE7trIuqQ2NkKl4zVVv3kijiPo2GZqT6MqFZuiIsw`
       );
       console.log(response.data.body);
       setListings(response.data.body);
@@ -83,15 +78,6 @@ const InvestmentListings = () => {
             Sort By Recent:
             <input type="checkbox" checked={sortByRecent} onChange={handleSortByRecentChange} />
           </label>
-          <label>
-            Property Type:
-            <select value={propertyType} onChange={handlePropertyTypeChange}>
-              <option value="none">Select</option>
-              <option value="hotel">Hotel</option>
-              <option value="apartments">Apartments</option>
-              <option value="condominium">Condominium</option>
-            </select>
-          </label>
           <button className="update-btn" onClick={fetchData}>Update</button>
         </div>
       </div>
@@ -100,19 +86,26 @@ const InvestmentListings = () => {
           <div key={listing.id} className="listing">
             <img src={listing.photos.split('\n')[0]} alt={listing.name} className="listing__img" />
             <h5 className="listing__name">{listing.name}</h5>
-            <div className="listing__location listing__detail">
-              <p>Location</p>
-              <p className="listing__value">{listing.location}</p>
+            <div className="listing__top">
+              <p className="listing__developer">{listing.developer}</p>
             </div>
             <div className="listing__detail">
-              <p>Property Type</p>
-              <p className="listing__value">{listing.property_type}</p>
+              <p>Location</p>
+              <p className="listing__value">{listing.location}</p>
             </div>
             <div className="listing__detail">
               <p>Date Published</p>
               <p className="listing__value">{reformatDate(listing.date_published)}</p>
             </div>
-            <div className="listing__price listing__detail">
+            <div className="listing__detail">
+              <p>Target Hold Period</p>
+              <p className="listing__value">{listing.target_hold_period}</p>
+            </div>
+            <div className="listing__detail">
+              <p>Interest Rate</p>
+              <p className="listing__value">{(listing.interest_rate*100).toFixed(1)}%</p>
+            </div>
+            <div className="listing__detail listing__bottom">
               <p>Minimum Investment</p>
               <p className="listing__value">${numberWithCommas(listing.minimum_investment)}</p>
             </div>
