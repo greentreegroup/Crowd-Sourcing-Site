667fb585e1c403e99822009a579c1b3b09031109
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2024-07-11 11:26:54 -0700

    Update Chatbot.js

--- crowd-sourcing/src/Chatbot.js
+++ crowd-sourcing/src/Chatbot.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chatbot.css';
 
 const Chatbot = () => {
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsMinimized(!isMinimized);
+  };
+
   return (
     <div className="chatbot-container">
-      <iframe
-        src="https://copilotstudio.microsoft.com/environments/Default-6ffa26de-1846-4087-81e9-2a9ec717152f/bots/cr419_chatbot/webchat?__version__=2"
-        frameBorder="0"
-        className="chatbot-iframe"
-        title="Chatbot"
-      ></iframe>
+      <button className="chatbot-toggle" onClick={toggleChatbot}>
+        {isMinimized ? 'Open Chat' : 'Close Chat'}
+      </button>
+      {!isMinimized && (
+        <iframe
+          src="https://copilotstudio.microsoft.com/environments/Default-6ffa26de-1846-4087-81e9-2a9ec717152f/bots/cr419_chatbot/webchat?__version__=2"
+          frameBorder="0"
+          className="chatbot-iframe"
+          title="Chatbot"
+        ></iframe>
+      )}
     </div>
   );
 };
