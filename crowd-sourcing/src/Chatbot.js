e24b51a95f4405e2f205f8c4483050f2017fb409
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2024-07-11 11:05:01 -0700

    Update Chatbot.js

--- crowd-sourcing/src/Chatbot.js
+++ crowd-sourcing/src/Chatbot.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Chatbot.css';
 
 const Chatbot = () => {
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div className="chatbot-container">
       <iframe
         src="https://copilotstudio.microsoft.com/environments/Default-6ffa26de-1846-4087-81e9-2a9ec717152f/bots/cr419_chatbot/webchat?__version__=2"
         frameBorder="0"
-        style={{ width: '100%', height: '100%' }}
-        title="Copilot Chatbot"
+        className="chatbot-iframe"
+        title="Chatbot"
       ></iframe>
     </div>
   );
